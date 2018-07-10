362802328557a30435bf0c914774dfbd6d495f6f
abradle <you@example.com>
2018-07-10 15:29:21 +0100

    Remove openbabel dependency

--- duck/steps/prepare.py
+++ duck/steps/prepare.py
@@ -1,23 +1,17 @@
 import sys,os
-import openbabel
 from rdkit.Chem import AllChem
 from rdkit import Chem
 
 def prep_lig(mol_file,prefix):
-    # Get the charge
+    # Protonate
+    protonated_mol = prefix + "_hs.mol"
     rd_mol = Chem.MolFromMolFile(mol_file)
-    net_charge = AllChem.GetFormalCharge(rd_mol)
-    mol2_file = prefix + ".mol2"
+    rd_mol = AllChem.AddHs(rd_mol,addCoords=True)
+    Chem.MolToMolFile(rd_mol,protonated_mol)
+    # Defined the final out file
     out_file = prefix + "_params.mol2"
     frcmod_file = prefix + ".frcmod"
-    # Conver the molecule
-    obConversion = openbabel.OBConversion()
-    obConversion.SetInAndOutFormats("mdl", "mol2")
-    mol = openbabel.OBMol()
-    obConversion.ReadFile(mol, mol_file)
-    mol.AddHydrogens()
-    obConversion.WriteFile(mol, mol2_file)
-    os.system("antechamber -i " + mol2_file + " -fi mol2 -o " + out_file + " -fo mol2 -at sybyl  -c bcc")
+    os.system("antechamber -i " + protonated_mol + " -fi mdl -o " + out_file + " -fo mol2 -at sybyl  -c bcc")
     return [out_file]
 
 if __name__ == "__main__":
