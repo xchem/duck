11a6456c88df03fc6017d98a3de8145f2cb48342
abradle <you@example.com>
2018-07-12 23:38:23 +0100

    Readd maciej

--- scripts/get_wqb.py
+++ scripts/get_wqb.py
@@ -19,6 +19,48 @@ def get_wqb_simple(file_duck_dat):
 
 # TODO Improved method for finding WQB
 
+def get_Wqb_value(file_duck_dat):
+    f = open(file_duck_dat, 'r')
+    data = []
+    for line in f:
+        a = line.split()
+        data.append([float(a[1]), float(a[3]), float(a[5]), float(a[8])])
+    f.close()
+    data = np.array(data[1:])
+    Work = data[:, 3]
+    seg_size = 50
+    # split it into segments of seg_size points
+    num_segments = int(len(data) / seg_size)
+    # alayze each segment to see if minimum in the segment is the local minimum
+    # local minimum is the point with the lowest value of 200 neighbouring points
+    # first local minumum is miminum used later to duck analysis
+    for segment in range(num_segments):
+        # detecting minium inthe segment
+        sub_data = data[segment * seg_size: (segment + 1) * seg_size]
+        sub_Work = sub_data[:, 3]
+        index_local = np.argmin(sub_Work)
+        # segment of  points arround detected minimum
+        index_global = index_local + segment * seg_size
+        if index_global > seg_size / 2:
+            sub2_data = data[index_global - seg_size / 2: index_global + seg_size / 2 + 1]
+        else:
+            sub2_data = data[0: index_global + seg_size / 2 + 1]
+        sub2_Work = sub2_data[:, 3]
+        index_local2 = np.argmin(sub2_Work)
+        if index_global < seg_size / 2:
+            if index_local2 == index_global:
+                Wqb_min_index = index_global
+            break
+        else:
+            if index_local2 == seg_size / 2:
+                Wqb_min_index = index_global
+    Wqb_min = Work[Wqb_min_index]
+    sub_max_data = data[Wqb_min_index:]
+    sub_max_Work = sub_max_data[:, 3]
+    Wqb_max = max(sub_max_Work)
+    Wqb_value = Wqb_max - Wqb_min
+    return (Wqb_value, data, Wqb_min)
+
 def get_Wqb_value_all(input_dir):
     file_list = []
     for fil in os.listdir(input_dir):
@@ -28,7 +70,7 @@ def get_Wqb_value_all(input_dir):
     Wqb_values = []
     plt.figure(figsize = (7,7))
     for fil in file_list:
-        Wqb_data = get_wqb_simple(fil)
+        Wqb_data = get_Wqb_value(fil)
         Wqb_values.append(Wqb_data[0])
         plt.plot(1*Wqb_data[1][:,0], Wqb_data[1][:,3]-Wqb_data[2])
 
