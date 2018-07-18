bfd150f0ccf484c40f0bd7c9ee77a03b0bf9c352
abradle <you@example.com>
2018-07-18 11:22:29 +0100

    Update script to go from yaml

--- scripts/do_chunk.py
+++ scripts/do_chunk.py
@@ -1,5 +1,5 @@
 from duck.steps.chunk import chunk_with_amber,do_tleap,remove_prot_buffers_alt_locs,find_disulfides
-import sys
+import sys,yaml
 
 def main(prot_file, mol_file, interaction, cutoff):
     # A couple of file name
@@ -16,4 +16,17 @@ def main(prot_file, mol_file, interaction, cutoff):
     do_tleap(chunk_protein, chunk_protein_prot, disulfides)
 
 if __name__ == "__main__":
-    main(sys.argv[1],sys.argv[2],sys.argv[3],int(sys.argv[4]))
+    # Define these in a YAML
+    out_data = yaml.load(open(sys.argv[1]))
+    prot_code = out_data["prot_code"]
+    prot_int = out_data["prot_int"]
+    # Cutoff for the sphere
+    cutoff = out_data["cutoff"]
+    md_len = out_data["md_len"]
+    init_velocity = out_data["init_velocity"]
+    num_smd_cycles = out_data["num_smd_cycles"]
+    gpu_id = str(out_data["gpu_id"])
+    # Now get the data from Fragalysis
+    prot_file = out_data["apo_pdb_file"]
+    mol_file = out_data["mol_file"]
+    main(prot_file,mol_file,prot_int,cutoff)
