fd78470e54fd54bdd6e3a97ab12876961b99d22e
abradle <you@example.com>
2018-07-19 11:00:36 +0100

    Use ready protonated ligands

--- duck/steps/prepare.py
+++ duck/steps/prepare.py
@@ -3,19 +3,13 @@ from rdkit.Chem import AllChem
 from rdkit import Chem
 
 def prep_lig(mol_file,prefix):
-    # Protonate
-    protonated_mol = prefix + "_hs.mol"
-    rd_mol = Chem.MolFromMolFile(mol_file)
-    rd_mol = AllChem.AddHs(rd_mol,addCoords=True)
-    # Remove aliases - otherwise charge isn't seen by antechamber.. (sigh)
-    for atom in rd_mol.GetAtoms():
-        atom.SetProp('molFileAlias', '')
-    Chem.MolToMolFile(rd_mol,protonated_mol)
+    # Accept readily protonated molecule
+    rd_mol = Chem.MolFromMolFile(mol_file,removeHs=False)
     net_charge = AllChem.GetFormalCharge(rd_mol)
     # Defined the final out file
     out_file = prefix + "_params.mol2"
     frcmod_file = prefix + ".frcmod"
-    os.system("antechamber -i " + protonated_mol + " -fi mdl -o " + out_file + " -fo mol2 -at sybyl  -c bcc -nc "+str(net_charge))
+    os.system("antechamber -i " + mol_file + " -fi mdl -o " + out_file + " -fo mol2 -at sybyl  -c bcc -nc "+str(net_charge))
     return [out_file]
 
 if __name__ == "__main__":
