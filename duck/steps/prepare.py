d8b08e781683437f043ef28321d34c24397addf2
abradle <you@example.com>
2018-07-27 16:36:27 +0100

    Save the h_added file

--- duck/steps/prepare.py
+++ duck/steps/prepare.py
@@ -5,13 +5,15 @@ from rdkit import Chem
 def prep_lig(mol_file,prefix,addHs=True):
     # Accept readily protonated molecule
     rd_mol = Chem.MolFromMolFile(mol_file,removeHs=False)
+    h_file = mol_file.replace(".mol","_h.mol")
     # Add extra hydrogens
     rd_mol = AllChem.AddHs(rd_mol, addCoords=True)
+    Chem.MolToMolFile(rd_mol,h_file)
     net_charge = AllChem.GetFormalCharge(rd_mol)
     # Defined the final out file
     out_file = prefix + "_params.mol2"
     frcmod_file = prefix + ".frcmod"
-    os.system("antechamber -i " + mol_file + " -fi mdl -o " + out_file + " -fo mol2 -at sybyl  -c bcc -nc "+str(net_charge))
+    os.system("antechamber -i " + h_file + " -fi mdl -o " + out_file + " -fo mol2 -at sybyl  -c bcc -nc "+str(net_charge))
     return [out_file]
 
 if __name__ == "__main__":
